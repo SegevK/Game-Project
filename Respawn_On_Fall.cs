1716b60eb3337920ba91187552bbee0773da8e7d
SegevK <55542193+SegevK@users.noreply.github.com>
2020-02-04 08:51:58 +0200

    Game Scripts Update

---
+++ Respawn_On_Fall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn_On_Fall : MonoBehaviour
+{
+   
+    public GameObject objToTP;
+    public Transform tpLoc;
+    
+
+    void OnTriggerStay(Collider other)
+    {
+     
+        if (other.gameObject.tag == "Player") 
+        {
+            objToTP.transform.position = tpLoc.transform.position;
+        }
+    }
+   
+}
